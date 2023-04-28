ca132ef335b3e569ba2a104423ce0c2b38e14e2e
Nikita Samsonenkov <mrkimatsuki@gmail.com>
2023-04-28 19:03:38 +0500

    Commit #2

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-content = {"title": 734764, "text": "text for 734764"}
+content = [
+    {"title": "Название для шаблона из словаря", "text": "Текст для шаблона из словаря"},
+    {"title": "Экс-глава регионального СК назначен главным федеральным инспектором", "text": "Бывший руководитель следственного управления Следственного комитета по Свердловской области Михаил Богинский назначен главным федеральным инспектором региона. Как сообщили в пресс-службе полпредства, соответствующее распоряжение подписал полномочный представитель президента в УрФО Владимир Якушев."},
+    {"title": "Посол Швейцарии выступил в защиту отказа от передачи Украине боеприпасов", "text": "Посол Швейцарии в Берлине Пауль Зегер выступил в защиту решения правительства страны об отказе передавать Украине боеприпасы швейцарского производства. О своей позиции он заявил изданию Augsburger Allgemeine."}
+]
 news_list = [
     {"title": 734764, "text": "text for 734764"},
     {"title": 734764, "text": "text for 734764"},
@@ -12,7 +16,7 @@ news_list = [
 
 
 def index():
-    return render_template("index.html", **content)
+    return render_template("index.html", content=content)
 
 
 def news():
@@ -20,7 +24,7 @@ def news():
 
 
 def news_detail(id):
-    return render_template("news_detail.html", **news_list[id])
+    return render_template("news_detail.html", **content[id])
 
 
 def category(name):
