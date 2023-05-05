82afd3bb06718649edd9ae483609e2744e7f046d
Nikita Samsonenkov <mrkimatsuki@gmail.com>
2023-05-05 19:21:30 +0500

    Commit #6

--- main.py
+++ main.py
@@ -39,6 +39,15 @@ class News(db.Model):
     date = db.Column(db.DateTime, default=datetime.utcnow())
 
 
+class Feedback(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), unique=False, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(200), nullable=True)
+    rating = db.Column(db.Integer, nullable=False, default=5)
+    date = db.Column(db.DateTime, default=datetime.utcnow())
+
+
 with app.app_context():
     db.create_all()
 
@@ -53,12 +62,15 @@ def feedback():
     form = FeedbackForm()
 
     if form.validate_on_submit():
-        name = form.name.data
-        text = form.text.data
-        email = form.email.data
-        rating = form.rating.data
+        feedback_model = Feedback()
 
-        print(name, text, email, rating, sep="\n\n---------------------\n\n")
+        feedback_model.name = form.name.data
+        feedback_model.text = form.text.data
+        feedback_model.email = form.email.data
+        feedback_model.rating = form.rating.data
+
+        db.session.add(feedback_model)
+        db.session.commit()
 
         return redirect(url_for("index"))
 
